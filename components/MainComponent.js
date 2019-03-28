1ec2738078fe47dc8fa5a8435b1ba88814b7d967
farissudofy <faris@sudofy.com>
2019-03-28 13:35:19 +0500

    Modal

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,6 +9,7 @@ import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView 
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
 
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
@@ -27,6 +28,24 @@ const CustomDrawerContentComponent = (props) => (
     </SafeAreaView>
   </ScrollView>
 );
+
+
+const ReservationNavigator = createStackNavigator({
+  Reservation: { screen: Reservation }
+}, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        paddingLeft: 10,
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()} />
+
+    })
+  })
 
 const MenuNavigator = createStackNavigator({
   Menu: {
@@ -137,6 +156,22 @@ const MainNavigator = createDrawerNavigator({
       title: 'Contact Us',
       drawerLabel: 'Contact Us',
       drawerIcon: ({ tintColor }) => <Icon name='address-card' type='font-awesome' size={22} color={tintColor} />
+    }
+  },
+  Reservation:
+  {
+    screen: ReservationNavigator,
+    navigationOptions: {
+      title: 'Reserve Table',
+      drawerLabel: 'Reserve Table',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='cutlery'
+          type='font-awesome'
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
     }
   },
 }, {
